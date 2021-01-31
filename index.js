42be6cfd2f06b0232faedbbf3699167d184e6fc8
Tung Pham <tpham20908@gmail.com>
2021-01-30 23:10:46 -0500

    complete v1

--- index.js
+++ index.js
@@ -2,19 +2,26 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoute from './routes/posts.js';
 
+dotenv.config();
+const { USER_NAME, USER_PASSWORD, COLLECTION_NAME } = process.env;
 const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
+app.get('/', (req, res) => {
+	res.send('Hello to memories_tp20908 API');
+});
+
 app.use('/posts', postRoute);
 
-const CONNECTION_URL =
-	'mongodb+srv://tung-102:xCoaJL7bmOas6aRh@cluster0.p8fkg.gcp.mongodb.net/memories?retryWrites=true&w=majority';
+const CONNECTION_URL = `mongodb+srv://${USER_NAME}:${USER_PASSWORD}@cluster0.p8fkg.gcp.mongodb.net/${COLLECTION_NAME}?retryWrites=true&w=majority`;
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
